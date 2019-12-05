e057f123dfe8f03eb79f3f95caef581c74bd6783
Sjors de Wit <dev@sjorsdewit.name>
2019-12-05 19:01:04 +0100

    Make static assert compatible with other version of c_utils

--- c_utils/static_assert.h
+++ c_utils/static_assert.h
@@ -15,14 +15,14 @@
 /* These can't be used after statements in c89. */
 #ifdef __COUNTER__
 #define STATIC_ASSERT(e) \
-	    enum { ASSERT_CONCAT(static_assert_, __COUNTER__) = 1/(!!(e)) }
+	    enum { ASSERT_CONCAT(c_utils_static_assert_, ASSERT_CONCAT(__COUNTER__, __LINE__)) = 1/(!!(e)) }
 #else
 /* This can't be used twice on the same line so ensure if using in headers
  *    * that the headers are not included twice (by wrapping in #ifndef...#endif)
  *       * Note it doesn't cause an issue when used on same line of separate modules
  *          * compiled with gcc -combine -fwhole-program.  */
 #define STATIC_ASSERT(e) \
-	    enum { ASSERT_CONCAT(assert_line_, __LINE__) = 1/(!!(e)) }
+	    enum { ASSERT_CONCAT(c_utils_static_assert_, __LINE__) = 1/(!!(e)) }
 #endif
 
 #endif
