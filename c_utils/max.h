b3aadc9d908e9c43225147a6d8fa299b5e8fc4ff
Sjors de Wit <dev@sjorsdewit.name>
2019-12-05 16:51:07 +0100

    Move min(a,b) to min.h
    If you want to use min(), #include <c_utils/min.h>.
    For backwards compatibility, #include <c_utils/max.h> will still work.

--- c_utils/max.h
+++ c_utils/max.h
@@ -6,10 +6,7 @@
      __typeof__ (b) _b = (b); \
      _a > _b ? _a : _b; })
 
-#define min(a,b) \
-    ({ __typeof__ (a) _a = (a); \
-     __typeof__ (b) _b = (b); \
-     _a < _b ? _a : _b; })
+#include "min.h"
 
 #endif
 
---
+++ c_utils/min.h
@@ -0,0 +1,10 @@
+#ifndef MIN_H
+#define MIN_H
+
+#define min(a,b) \
+    ({ __typeof__ (a) _a = (a); \
+     __typeof__ (b) _b = (b); \
+     _a < _b ? _a : _b; })
+
+#endif
+
